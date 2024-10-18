65cb1783fb9788a9f8ab62ef0118b1a6f026a9ae
mohithgg <166399043+mohithgg@users.noreply.github.com>
2024-10-18 17:40:37 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,30 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-require('dotenv').config()
-
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const walletAddress = ""; // place your public address for your wallet here
+const { ethers } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
 
-    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-  
-    const tx = await token.mint(walletAddress, 1000);
-    await tx.wait();
+  const privateKey = process.env.PRIVATE_KEY;
 
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+  const networkAddress = "https://ethereum-sepolia-rpc.publicnode.com";
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  const contractAddress = "0x58752A414F876DF0A5b931Dc6D1f7b2936ea9B19";
+
+  const Wetton = await ethers.getContractFactory("Belgium", signer);
+  const Belgium_contract = await Wetton.attach(contractAddress);
+
+  await Belgium_contract.mint(5);
+
+  console.log("Succesfully 5 NFTs Minted");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
